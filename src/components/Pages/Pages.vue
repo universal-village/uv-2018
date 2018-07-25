e76b5e294f9f8adcfeb6437aae076ec8c8150583
Paradise02 <971599473@qq.com>
2018-07-25 10:53:04 -0500

    update

--- src/components/Pages/Pages.vue
+++ src/components/Pages/Pages.vue
@@ -1,12 +1,12 @@
 <template>
-  <a-layout-content style="padding: 0 50px">
+  <a-layout-content class="main-content">
     <a-layout style="padding: 0 24px 24px; background: rgba(255, 255, 255, .75);">
       <a-breadcrumb style="margin: 16px 0">
         <a-breadcrumb-item v-for="navPage in this.$store.state.navPageList" :key="navPage">
           {{ navPage | capitalize }}
         </a-breadcrumb-item>
       </a-breadcrumb>
-      <a-layout-content :style="{ padding: '24px', margin: 0, minHeight: '280px' }" v-html="pagePlain">
+      <a-layout-content v-html="pagePlain" class="page-content">
       </a-layout-content>
     </a-layout>
   </a-layout-content>
@@ -78,11 +78,55 @@ export default {
   li{
     font-size: 16px;
   }
-  p {
-    font-size: 16px;
-  }
-
   a:link {
     text-decoration: underline;
+  }
+
+  .ant-layout-content p {
+    font-size: 1.2em;
+    font-weight: bold;
+    font-family: "Times New Roman", Times, serif;
+  }
+
+  @media only screen and (max-width: 960px) {
+      .ant-layout-content {
+      padding: 0 20px;
+    }
+  }
+  @media only screen and (min-width: 960px){
+      .ant-layout-content.main-content {
+        padding: 0 10%;
+    }
+      .ant-layout {
+        padding: 0 24px 24px;
+    }
+      /* .ant-collapse-item  {
+        font-size: 1.5em !important;
+        font-weight: bolder;
+      } */
+  }
+</style>
+
+<style type="text/css">
+  .page-content p {
+    font-size: 1.2em;
+    font-weight: bold;
+    font-family: "Times New Roman", Times, serif;
+  }
+
+  .page-content h1 {
+    font-weight: bolder;
+  }
+
+  .page-content h2 {
+    font-weight: bolder;
+  }
+
+  .page-content h3 {
+    font-weight: bolder;
+  }
+
+  .page-content li {
+    font-weight: bolder;
   }
 </style>
--- src/components/Success.vue
+++ src/components/Success.vue
@@ -1,9 +1,35 @@
-<template lang="html">
-
-  <section class="success">
-    <h1>success Component</h1>
-  </section>
-
+<template>
+  <a-layout-content class="main-content">
+    <a-layout style="padding: 0 24px 24px; background: rgba(255, 255, 255, .75);">
+      <a-breadcrumb style="margin: 16px 0">
+        <a-breadcrumb-item v-for="navPage in this.$store.state.navPageList" :key="navPage">
+          {{ navPage | capitalize }}
+        </a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-layout-content class="page-content">
+        <a-card
+          hoverable
+          style="width: 36%"
+        >
+          <img
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            slot="cover"
+          />
+          <!-- <ul class="ant-card-actions" slot="actions">
+            <li style="width: 33.3333%;"><a-icon type="setting" /></li>
+            <li style="width: 33.3333%;"><a-icon type="edit" /></li>
+            <li style="width: 33.3333%;"> <a-icon type="ellipsis" /></li>
+          </ul> -->
+          <a-card-meta
+            title="Card title"
+            description="This is the description">
+            <a-avatar slot="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          </a-card-meta>
+        </a-card>
+      </a-layout-content>
+    </a-layout>
+  </a-layout-content>
 </template>
 
 <script lang="js">
@@ -28,7 +54,21 @@ export default {
 </script>
 
 <style scoped lang="css">
-  .success {
-
+  @media only screen and (max-width: 960px) {
+      .ant-layout-content {
+      padding: 0 20px;
+    }
+  }
+  @media only screen and (min-width: 960px){
+      .ant-layout-content.main-content {
+        padding: 0 15%;
+    }
+      .ant-layout {
+        padding: 0 24px 24px;
+    }
+      /* .ant-collapse-item  {
+        font-size: 1.5em !important;
+        font-weight: bolder;
+      } */
   }
 </style>
