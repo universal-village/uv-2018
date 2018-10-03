c287f2a41608e5c5965bc85b7422c3b3191e325c
GalvinGao <GalvinGao@users.noreply.github.com>
2018-10-04 00:01:23 +0800

    U: Donation

--- src/components/My/Donation.vue
+++ src/components/My/Donation.vue
@@ -6,29 +6,11 @@
           {{ navPage | capitalize }}
         </a-breadcrumb-item>
       </a-breadcrumb>
-      <a-layout-content>
+      <a-layout-content class="donation-page">
         <h1>Donation</h1>
-        <a-form @submit="submitHandler" :autoFormCreate="(form)=>{this.form = form}">
-          <a-form-item label="Amount" :labelCol="{ span: 5 }" :wrapperCol="{ span: 12 }" fieldDecoratorId="amount"
-                       :fieldDecoratorOptions="{rules: [{ required: true, message: 'Please specify your donation amount!' }]}">
-            <a-radio-group v-model="amountType" default-value="1000000" @change="donationAmountHandler" button-style="solid">
-              <a-radio-button class="monospace" value="presets|1000000">$ 10,000.00</a-radio-button>
-              <a-radio-button class="monospace" value="presets|500000">$ 5,000.00</a-radio-button>
-              <a-radio-button class="monospace" value="presets|300000">$ 3,000.00</a-radio-button>
-              <a-radio-button class="monospace" value="custom">Other...</a-radio-button>
-            </a-radio-group>
-          </a-form-item>
-          <a-form-item label="Custom Amount" size="large" :labelCol="{ span: 5 }" :wrapperCol="{ span: 6 }" fieldDecoratorId="customAmount" v-if="customAmount">
-            <a-input class="monospace" placeholder="Please enter donation amount..." @change="customAmountHandler" ref="amountInput">
-              <a-icon slot="prefix" type="pay-circle"/>
-            </a-input>
-          </a-form-item>
-          <a-form-item>
-            <a-button type="primary" htmlType="submit">
-              Submit
-            </a-button>
-          </a-form-item>
-        </a-form>
+        <a-table :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}" :columns="columns"
+                 :dataSource="donationConfiguration" :pagination="false"/>
+        choosed #{{selectedRowKeys[0]}}, have to pay {{donationConfiguration[selectedRowKeys[0] - 1].price}}
       </a-layout-content>
     </a-layout>
   </a-layout-content>
@@ -37,7 +19,51 @@
 <script lang="js">
 import ARadioGroup from 'vue-antd-ui/es/radio/Group'
 import AFormItem from 'vue-antd-ui/es/form/FormItem'
-import AButton from "vue-antd-ui/es/button/button";
+import AButton from 'vue-antd-ui/es/button/button'
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+}, {
+  title: 'Benefits',
+  dataIndex: 'benefits',
+}, {
+  title: 'Price (USD, total contribution)',
+  dataIndex: 'priceDescription',
+}]
+
+const donationConfiguration = [{
+  key: '1',
+  name: 'Platinum Sponsor',
+  benefits: `A lot of description here`,
+  priceDescription: '> $ 10,000',
+  price: '1000000'
+}, {
+  key: '2',
+  name: 'Gold Sponsor',
+  benefits: `A lot of description here`,
+  priceDescription: '> $ 6,000',
+  price: '600000'
+}, {
+  key: '3',
+  name: 'Silver Sponsor',
+  benefits: `A lot of description here`,
+  priceDescription: '> $ 3,000',
+  price: '300000'
+}, {
+  key: '4',
+  name: 'Conference Exhibitor',
+  benefits: `A lot of description here`,
+  priceDescription: '= $ 3,000 - Subject to Organizer Approval',
+  price: '300000'
+}, {
+  key: '5',
+  name: 'Other',
+  benefits: `A lot of description here`,
+  priceDescription: '< $ 3,000',
+  price: '100000'
+}]
+
 export default {
   name: 'donation',
   components: {AButton, AFormItem, ARadioGroup},
@@ -47,8 +73,10 @@ export default {
   },
   data () {
     return {
-      amountType: null,
-      customAmount: false
+      donationConfiguration,
+      columns,
+      selectedRowKeys: [], // Check here to configure the default column
+      loading: false,
     }
   },
   methods: {
@@ -74,10 +102,15 @@ export default {
     },
     customAmountHandler (event) {
       console.log(event)
+    },
+    onSelectChange (selectedRowKey) {
+      console.log('selectedRowKey: ', selectedRowKey, '|', selectedRowKey[selectedRowKey.length - 1])
+      let tempList = []
+      tempList[0] = selectedRowKey[selectedRowKey.length - 1]
+      this.selectedRowKeys = tempList
     }
   },
   computed: {
-
   }
 }
 </script>
@@ -86,6 +119,7 @@ export default {
   h1 {
     color: green;
   }
+
   h2 {
     display: inline;
     color: green;
@@ -93,16 +127,20 @@ export default {
     margin-left: 10px;
     font-family: Verdana;
   }
+
   h3 {
-    color:green;
+    color: green;
   }
+
   h4 {
     color: green;
     font-size: 16px;
   }
-  li{
+
+  li {
     font-size: 16px;
   }
+
   a:link {
     text-decoration: underline;
   }
@@ -114,24 +152,33 @@ export default {
   }
 
   @media only screen and (max-width: 960px) {
-      .ant-layout-content {
+    .ant-layout-content {
       padding: 0 20px;
     }
   }
-  @media only screen and (min-width: 960px){
-      .ant-layout-content.main-content {
-        padding: 0 10%;
+
+  @media only screen and (min-width: 960px) {
+    .ant-layout-content.main-content {
+      padding: 0 10%;
     }
-      .ant-layout {
-        padding: 0 24px 24px;
+
+    .ant-layout {
+      padding: 0 24px 24px;
     }
-      /* .ant-collapse-item  {
-        font-size: 1.5em !important;
-        font-weight: bolder;
-      } */
+
+    /* .ant-collapse-item  {
+      font-size: 1.5em !important;
+      font-weight: bolder;
+    } */
   }
 
   .monospace {
     font-family: Consolas, Menlo, Courier, monospace;
+  }
+</style>
+
+<style>
+  .ant-table-selection {
+    display: none;
   }
 </style>
